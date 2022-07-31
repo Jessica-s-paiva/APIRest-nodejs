6e55769ce17b78a956385766f72cc000c9daedaa
Jessica-s-paiva <jessicapaivamvp@gmail.com>
2022-07-31 19:48:53 -0300

    feat :add verbo get da rota produtos

---
+++ src/Model/Produto.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const produtoSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true},
+        cor: { type: String, required: true },
+        marca: { type: String, required: true },
+        peso: { type: String, required: true },
+        tamanho: { type: String, required: true },
+        valor: { type: String, required: true },
+        descricao: { type: String, required: true }
+    }
+);
+
+const produtos = mongoose.model('produtos', produtoSchema);
+
+export default produtos;
--- src/app.js
+++ src/app.js
@@ -1,40 +1,45 @@
 import express from "express";
 import cors from "cors";
+import db from "./Config/dbConnect.js";
+import produtos from "./Model/Produto.js";
+
+db.on("error", console.log.bind(console, 'Erro de conexão'))
+db.once("open", () => {
+  console.log('conexão com o banco feita com sucesso')
+});
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-const livros = [
-  {id: 1, "titulo": "Senhor dos Aneis"},
-  {id: 2, "titulo": "O Hobbit"}
-]
 
 app.get('/', (req, res) => {
   res.status(200).send('Curso de Node');
 }) 
 
-app.get('/livros', (req, res) => {
-  res.status(200).json(livros);
+app.get('/produtos', (req, res) => {
+  produtos.find((err, produtos) => { 
+    res.status(200).json(produtos)
+  });
 })
 
-app.get('/livros/:id', (req, res) => {
+app.get('/produtos/:id', (req, res) => {
     let index = buscaLivro(req.params.id);
     res.json(livros[index]);
   })
 
-app.post('/livros', (req, res) => {
+app.post('/produtos', (req, res) => {
     livros.push(req.body);
     res.status(201).send('Livro foi cadastrado com sucesso');
 })
 
-app.put('/livros/:id', (req, res) => {
+app.put('/produtos/:id', (req, res) => {
     let index = buscaLivro(req.params.id);
     livros[index].titulo = req.body.titulo;
     res.json(livros);
 })
 
-app.delete('/livros/:id', (req, res) => {
+app.delete('/produtos/:id', (req, res) => {
     let {id} = req.params;
     let index = buscaLivro(id);
     livros.splice(index, 1);
